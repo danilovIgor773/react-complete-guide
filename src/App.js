d8bf81955793655bb7a45da179da26066b78a2fd
Danilov Igor <danilovigor773@gmail.com>
2019-07-09 18:12:32 +0500

    Added 'state' obj for main App class to customize components dynamicly

--- src/App.js
+++ src/App.js
@@ -1,24 +1,37 @@
-import React from "react";
+import React, { Component } from "react";
 import "./App.css";
 import Person from "./Person/Person";
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Hi I''am a React App!</h1>
-      <Person />
-    </div>
-  );
-}
+class App extends Component {
+  state = {
+    persons: [
+      { name: "Igor", age: "29" },
+      { name: "Tim", age: "30" },
+      { name: "Rem", age: "20" }
+    ]
+  };
 
-// class App extends Component {
-//   render() {
-//     return React.createElement(
-//       "div",
-//       { className: "App" },
-//       React.createElement("h1", null, "This component created in other way!")
-//     );
-//   }
-// }
+  render() {
+    return (
+      <div className="App">
+        <h1>Hi I''am a React App!</h1>
+        <Person
+          name={this.state.persons[0].name}
+          age={this.state.persons[0].age}
+        />
+        <Person
+          name={this.state.persons[1].name}
+          age={this.state.persons[1].age}
+        >
+          My hobbies is: Racing
+        </Person>
+        <Person
+          name={this.state.persons[2].name}
+          age={this.state.persons[2].age}
+        />
+      </div>
+    );
+  }
+}
 
 export default App;
