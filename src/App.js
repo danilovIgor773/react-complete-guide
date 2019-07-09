a72170731e1c36244c3588375b025df90f848cdf
Danilov Igor <danilovigor773@gmail.com>
2019-07-09 16:30:12 +0500

    Added 'Person'test-component

--- src/App.js
+++ src/App.js
@@ -1,22 +1,24 @@
-import React, { Component } from "react";
+import React from "react";
 import "./App.css";
+import Person from "./Person/Person";
 
-// function App() {
-//   return (
-//     <div className="App">
-//       <h1>Hi I''am a React App!</h1>
-//     </div>
-//   );
-// }
-
-class App extends Component {
-  render() {
-    return React.createElement(
-      "div",
-      { className: "App" },
-      React.createElement("h1", null, "This component created in other way!")
-    );
-  }
+function App() {
+  return (
+    <div className="App">
+      <h1>Hi I''am a React App!</h1>
+      <Person />
+    </div>
+  );
 }
+
+// class App extends Component {
+//   render() {
+//     return React.createElement(
+//       "div",
+//       { className: "App" },
+//       React.createElement("h1", null, "This component created in other way!")
+//     );
+//   }
+// }
 
 export default App;
