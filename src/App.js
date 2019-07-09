075ec915cbceacfc1f4060e6434870a81103bdda
Danilov Igor <danilovigor773@gmail.com>
2019-07-09 15:36:59 +0500

    init project env

--- src/App.js
+++ src/App.js
@@ -1,26 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// function App() {
+//   return (
+//     <div className="App">
+//       <h1>Hi I''am a React App!</h1>
+//     </div>
+//   );
+// }
+
+class App extends Component {
+  render() {
+    return React.createElement(
+      "div",
+      { className: "App" },
+      React.createElement("h1", null, "This component created in other way!")
+    );
+  }
 }
 
 export default App;
