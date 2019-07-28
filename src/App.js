608a1a96ceb6a067cbe8462b8013098f3627b9f9
Danilov Igor <danilovigor773@gmail.com>
2019-07-28 17:56:03 +0500

    Enabled css modules in webpack config

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import "./App.css";
+import styles from "./App.css";
 import Person from "./Person/Person";
 
 //import UserInput from "./UserInput/UserInput";
@@ -81,14 +81,14 @@ class App extends Component {
     let classes = [];
 
     if (this.state.persons.length <= 2) {
-      classes.push("red");
+      classes.push(styles.red);
     }
     if (this.state.persons.length <= 1) {
-      classes.push("bold");
+      classes.push(styles.bold);
     }
 
     return (
-      <div className="App">
+      <div className={styles.App}>
         <h1>Hi I''am a React App!</h1>
         <p className={classes.join(" ")}>This is really working!</p>
         <button style={styleButton} onClick={this.togglePersonsHandler}>
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,9 +1,9 @@
 import React from "react";
-import "./Person.css";
+import persStyle from "./Person.css";
 
 const person = props => {
   return (
-    <div className="Person">
+    <div className={persStyle.Person}>
       <p onClick={props.click}>
         I am {props.name} and I am {props.age} years old!
       </p>
